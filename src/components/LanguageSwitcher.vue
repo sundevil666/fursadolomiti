074d6723546bf5109b086098d48784a02fe35eef
sundevildi <sundevildi@gmail.com>
2026-07-06 12:14:23 +0200

    Update language switcher and styles

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -55,7 +55,23 @@ const selectLocale = (value: AppLocale) => {
           {{ currentLocale.toUpperCase() }}
         </span>
       </Transition>
-      <q-icon class="language-switcher__icon" name="expand_more" />
+      <!--      <q-icon class="language-switcher__icon" name="expand_more" />-->
+      <span class="flex align-center justify-center language-switcher__icon">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M6 9L12 15L18 9"
+            stroke="#08211F"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </span>
       <Transition name="language-spark">
         <span v-if="spark" :key="spark" class="language-switcher__spark" aria-hidden="true">
           <span />
