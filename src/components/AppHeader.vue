ad95b19167b8d1fa1fc2f7859cf242d38e6884bd
sundevildi <sundevildi@gmail.com>
2026-07-06 09:42:09 +0200

    Replace icon-right with custom SVG arrow for webcams nav item
    - Remove icon-right prop from q-btn
    - Add custom SVG chevron icon for webcams navigation
    - Simplify nav link styling transitions

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -93,9 +93,13 @@ onBeforeUnmount(() => {
           :class="{ 'app-header__nav-link--active': isActiveRoute(item.routeName) }"
           :style="{ '--nav-label-width': navLabelWidth(item) }"
           :to="{ name: item.routeName }"
-          :icon-right="item.id === 'webcams' ? 'expand_more' : undefined"
         >
           <AnimatedText :text="item.labels[currentLocale]" tag="span" />
+          <template v-if="item.id === 'webcams'">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 9L12 15L18 9" stroke="#3A6E61" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </template>
         </q-btn>
       </nav>
 
