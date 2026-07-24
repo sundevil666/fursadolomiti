871b1c28f5ca1d69d4e31ecc0568f76423329c28
sundevildi <sundevildi@gmail.com>
2026-07-24 11:39:48 +0200

    Update App.vue and main.scss

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { onBeforeUnmount, watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+watch(
+  () => route.name,
+  (routeName) => {
+    document.body.classList.toggle('main-page', routeName === 'home')
+  },
+  { immediate: true },
+)
+
+onBeforeUnmount(() => {
+  document.body.classList.remove('main-page')
+})
+</script>
+
 <template>
   <RouterView />
 </template>
