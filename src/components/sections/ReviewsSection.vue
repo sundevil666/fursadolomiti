4dcfe3b83bc77bc5773461a2adb3feffd58ea18b
sundevildi <sundevildi@gmail.com>
2026-08-09 08:54:23 +0200

    Update reviews section UI

--- src/components/sections/ReviewsSection.vue
+++ src/components/sections/ReviewsSection.vue
@@ -85,7 +85,8 @@ const handleReviewTouchEnd = (event: TouchEvent) => {
   reviewTouchStartX.value = null
 }
 
-const getReviewTranslation = (review: GuestReview) => review.translations?.[currentLocale.value]?.trim()
+const getReviewTranslation = (review: GuestReview) =>
+  review.translations?.[currentLocale.value]?.trim()
 
 const hasReviewTranslation = (review: GuestReview) => Boolean(getReviewTranslation(review))
 
@@ -104,7 +105,8 @@ const toggleReviewTranslation = (review: GuestReview) => {
   }
 }
 
-const isLongReview = (review: GuestReview) => getDisplayReviewText(review).length > reviewPreviewLength
+const isLongReview = (review: GuestReview) =>
+  getDisplayReviewText(review).length > reviewPreviewLength
 
 const getReviewText = (review: GuestReview) => {
   const text = getDisplayReviewText(review)
@@ -119,7 +121,8 @@ const getReviewLocation = (review: GuestReview) => review.location[currentLocale
 const getReviewDate = (review: GuestReview) => review.date[currentLocale.value]
 
 const openReviewModal = (review: GuestReview) => {
-  previouslyFocusedElement.value = document.activeElement instanceof HTMLElement ? document.activeElement : null
+  previouslyFocusedElement.value =
+    document.activeElement instanceof HTMLElement ? document.activeElement : null
   selectedReview.value = review
   pauseReviewAutoplay()
 
@@ -194,7 +197,20 @@ onBeforeUnmount(() => {
           :aria-label="t('home.reviews.previous')"
           @click="setReview(activeReviewIndex - 1)"
         >
-          <q-icon name="chevron_left" />
+          <svg
+            width="9"
+            height="17"
+            viewBox="0 0 9 17"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8.5 0.500001L0.5 8.5L8.5 16.5"
+              stroke="#EFE4C9"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </button>
 
         <Transition name="review-slide" mode="out-in">
@@ -225,14 +241,61 @@ onBeforeUnmount(() => {
                     :class="`reviews-section__social--${review.source}`"
                     aria-hidden="true"
                   >
-                    <span v-if="review.source === 'instagram'" class="reviews-section__instagram-mark" />
-                    <span v-else>G</span>
+                    <span
+                      v-if="review.source === 'instagram'"
+                      class="reviews-section__instagram-mark"
+                    >
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 1.8072C12.6506 1.8072 13.012 1.8072 14.0964 1.8072C15.0602 1.8072 15.5422 2.0482 15.9036 2.1687C16.3855 2.4096 16.747 2.5301 17.1084 2.8916C17.4699 3.253 17.7108 3.6145 17.8313 4.0964C17.9518 4.4578 18.0723 4.9398 18.1928 5.9036C18.1928 6.988 18.1928 7.2289 18.1928 10C18.1928 12.7711 18.1928 13.012 18.1928 14.0964C18.1928 15.0602 17.9518 15.5422 17.8313 15.9036C17.5904 16.3855 17.4699 16.747 17.1084 17.1084C16.747 17.4699 16.3855 17.7108 15.9036 17.8313C15.5422 17.9518 15.0602 18.0723 14.0964 18.1928C13.012 18.1928 12.7711 18.1928 10 18.1928C7.2289 18.1928 6.988 18.1928 5.9036 18.1928C4.9398 18.1928 4.4578 17.9518 4.0964 17.8313C3.6145 17.5904 3.253 17.4699 2.8916 17.1084C2.5301 16.747 2.2892 16.3855 2.1687 15.9036C2.0482 15.5422 1.9277 15.0602 1.8072 14.0964C1.8072 13.012 1.8072 12.7711 1.8072 10C1.8072 7.2289 1.8072 6.988 1.8072 5.9036C1.8072 4.9398 2.0482 4.4578 2.1687 4.0964C2.4096 3.6145 2.5301 3.253 2.8916 2.8916C3.253 2.5301 3.6145 2.2892 4.0964 2.1687C4.4578 2.0482 4.9398 1.9277 5.9036 1.8072C6.988 1.8072 7.3494 1.8072 10 1.8072ZM10 0C7.2289 0 6.988 0 5.9036 0C4.8193 0 4.0964 0.241 3.494 0.4819C2.8916 0.7229 2.2892 1.0843 1.6867 1.6867C1.0843 2.2892 0.8434 2.7711 0.4819 3.494C0.241 4.0964 0.1205 4.8193 0 5.9036C0 6.988 0 7.3494 0 10C0 12.7711 0 13.012 0 14.0964C0 15.1807 0.241 15.9036 0.4819 16.506C0.7229 17.1084 1.0843 17.7108 1.6867 18.3133C2.2892 18.9157 2.7711 19.1566 3.494 19.5181C4.0964 19.759 4.8193 19.8795 5.9036 20C6.988 20 7.3494 20 10 20C12.6506 20 13.012 20 14.0964 20C15.1807 20 15.9036 19.759 16.506 19.5181C17.1084 19.2771 17.7108 18.9157 18.3133 18.3133C18.9157 17.7108 19.1566 17.2289 19.5181 16.506C19.759 15.9036 19.8795 15.1807 20 14.0964C20 13.012 20 12.6506 20 10C20 7.3494 20 6.988 20 5.9036C20 4.8193 19.759 4.0964 19.5181 3.494C19.2771 2.8916 18.9157 2.2892 18.3133 1.6867C17.7108 1.0843 17.2289 0.8434 16.506 0.4819C15.9036 0.241 15.1807 0.1205 14.0964 0C13.012 0 12.7711 0 10 0Z" fill="#FFF6E1"/>
+<path d="M10 1.8072H14.0964C15.0602 1.8072 15.5422 2.0482 15.9036 2.1687C16.3855 2.4096 16.747 2.5301 17.1084 2.8916C17.4699 3.253 17.7108 3.6145 17.8313 4.0964C17.9518 4.4578 18.0723 4.9398 18.1928 5.9036V10V14.0964C18.1928 15.0602 17.9518 15.5422 17.8313 15.9036C17.5904 16.3855 17.4699 16.747 17.1084 17.1084C16.747 17.4699 16.3855 17.7108 15.9036 17.8313C15.5422 17.9518 15.0602 18.0723 14.0964 18.1928H10H5.9036C4.9398 18.1928 4.4578 17.9518 4.0964 17.8313C3.6145 17.5904 3.253 17.4699 2.8916 17.1084C2.5301 16.747 2.2892 16.3855 2.1687 15.9036C2.0482 15.5422 1.9277 15.0602 1.8072 14.0964V10V5.9036C1.8072 4.9398 2.0482 4.4578 2.1687 4.0964C2.4096 3.6145 2.5301 3.253 2.8916 2.8916C3.253 2.5301 3.6145 2.2892 4.0964 2.1687C4.4578 2.0482 4.9398 1.9277 5.9036 1.8072H10Z" fill="#FFF6E1"/>
+<path d="M15.1089 5C15.6612 5 16.1089 4.5523 16.1089 4C16.1089 3.4477 15.6612 3 15.1089 3C14.5566 3 14.1089 3.4477 14.1089 4C14.1089 4.5523 14.5566 5 15.1089 5Z" fill="#175445"/>
+<path d="M10 14C12.2091 14 14 12.2091 14 10C14 7.79086 12.2091 6 10 6C7.79086 6 6 7.79086 6 10C6 12.2091 7.79086 14 10 14Z" stroke="#175445" stroke-width="2"/>
+</svg>
+
+                    </span>
+                    <span v-else>
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M19.6 10.2271C19.6 9.51801 19.5364 8.83619 19.4182 8.18164H10V12.0498H15.3818C15.15 13.2998 14.4455 14.3589 13.3864 15.068V17.5771H16.6182C18.5091 15.8362 19.6 13.2726 19.6 10.2271Z"
+                          fill="#FFF6E1"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M9.99961 19.9999C12.6996 19.9999 14.9633 19.1045 16.6178 17.5772L13.386 15.0681C12.4905 15.6681 11.3451 16.0226 9.99961 16.0226C7.39506 16.0226 5.19051 14.2635 4.40415 11.8999H1.06323V14.4908C2.70869 17.759 6.09052 19.9999 9.99961 19.9999Z"
+                          fill="#FFF6E1"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M4.40455 11.8997C4.20455 11.2997 4.09092 10.6588 4.09092 9.99971C4.09092 9.34062 4.20455 8.6997 4.40455 8.0997V5.50879H1.06364C0.386364 6.85879 0 8.38607 0 9.99971C0 11.6133 0.386364 13.1406 1.06364 14.4906L4.40455 11.8997Z"
+                          fill="#FFF6E1"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M9.99961 3.97728C11.4678 3.97728 12.786 4.48183 13.8224 5.47274L16.6905 2.60455C14.9587 0.990911 12.6951 0 9.99961 0C6.09052 0 2.70869 2.24091 1.06323 5.5091L4.40415 8.10002C5.19051 5.73638 7.39506 3.97728 9.99961 3.97728Z"
+                          fill="#FFF6E1"
+                        />
+                      </svg>
+                    </span>
                   </span>
                   <span class="reviews-section__author-copy">
-                    <strong>
-                      <AnimatedText :text="`${review.author}, ${getReviewLocation(review)}`" tag="span" />
+                    <strong class="reviews-section__author">
+                      <AnimatedText
+                        :text="`${review.author}, ${getReviewLocation(review)}`"
+                        tag="span"
+                      />
                     </strong>
-                    <span>
+                    <span class="reviews-section__author-date">
                       <AnimatedText :text="getReviewDate(review)" tag="span" />
                     </span>
                   </span>
@@ -265,17 +328,34 @@ onBeforeUnmount(() => {
           :aria-label="t('home.reviews.next')"
           @click="setReview(activeReviewIndex + 1)"
         >
-          <q-icon name="chevron_right" />
+          <svg
+            width="9"
+            height="17"
+            viewBox="0 0 9 17"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0.499999 0.500001L8.5 8.5L0.5 16.5"
+              stroke="#EFE4C9"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </button>
 
-        <div v-if="totalReviewSlides > 1" class="reviews-section__dots" aria-hidden="true">
+        <div
+          v-if="totalReviewSlides > 1"
+          class="reviews-section__dots"
+          :style="{ '--review-dot-count': totalReviewSlides }"
+          aria-hidden="true"
+        >
           <button
             v-for="index in totalReviewSlides"
             :key="index"
             class="reviews-section__dot"
             :class="{ 'reviews-section__dot--active': index - 1 === activeReviewIndex }"
             :style="{
-              '--review-dot-count': totalReviewSlides,
               '--review-slide-duration': `${reviewAutoplayDelay}ms`,
             }"
             type="button"
@@ -323,11 +403,54 @@ onBeforeUnmount(() => {
                   <span
                     v-if="selectedReview.source === 'instagram'"
                     class="reviews-section__instagram-mark"
-                  />
-                  <span v-else>G</span>
+                  >
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 1.8072C12.6506 1.8072 13.012 1.8072 14.0964 1.8072C15.0602 1.8072 15.5422 2.0482 15.9036 2.1687C16.3855 2.4096 16.747 2.5301 17.1084 2.8916C17.4699 3.253 17.7108 3.6145 17.8313 4.0964C17.9518 4.4578 18.0723 4.9398 18.1928 5.9036C18.1928 6.988 18.1928 7.2289 18.1928 10C18.1928 12.7711 18.1928 13.012 18.1928 14.0964C18.1928 15.0602 17.9518 15.5422 17.8313 15.9036C17.5904 16.3855 17.4699 16.747 17.1084 17.1084C16.747 17.4699 16.3855 17.7108 15.9036 17.8313C15.5422 17.9518 15.0602 18.0723 14.0964 18.1928C13.012 18.1928 12.7711 18.1928 10 18.1928C7.2289 18.1928 6.988 18.1928 5.9036 18.1928C4.9398 18.1928 4.4578 17.9518 4.0964 17.8313C3.6145 17.5904 3.253 17.4699 2.8916 17.1084C2.5301 16.747 2.2892 16.3855 2.1687 15.9036C2.0482 15.5422 1.9277 15.0602 1.8072 14.0964C1.8072 13.012 1.8072 12.7711 1.8072 10C1.8072 7.2289 1.8072 6.988 1.8072 5.9036C1.8072 4.9398 2.0482 4.4578 2.1687 4.0964C2.4096 3.6145 2.5301 3.253 2.8916 2.8916C3.253 2.5301 3.6145 2.2892 4.0964 2.1687C4.4578 2.0482 4.9398 1.9277 5.9036 1.8072C6.988 1.8072 7.3494 1.8072 10 1.8072ZM10 0C7.2289 0 6.988 0 5.9036 0C4.8193 0 4.0964 0.241 3.494 0.4819C2.8916 0.7229 2.2892 1.0843 1.6867 1.6867C1.0843 2.2892 0.8434 2.7711 0.4819 3.494C0.241 4.0964 0.1205 4.8193 0 5.9036C0 6.988 0 7.3494 0 10C0 12.7711 0 13.012 0 14.0964C0 15.1807 0.241 15.9036 0.4819 16.506C0.7229 17.1084 1.0843 17.7108 1.6867 18.3133C2.2892 18.9157 2.7711 19.1566 3.494 19.5181C4.0964 19.759 4.8193 19.8795 5.9036 20C6.988 20 7.3494 20 10 20C12.6506 20 13.012 20 14.0964 20C15.1807 20 15.9036 19.759 16.506 19.5181C17.1084 19.2771 17.7108 18.9157 18.3133 18.3133C18.9157 17.7108 19.1566 17.2289 19.5181 16.506C19.759 15.9036 19.8795 15.1807 20 14.0964C20 13.012 20 12.6506 20 10C20 7.3494 20 6.988 20 5.9036C20 4.8193 19.759 4.0964 19.5181 3.494C19.2771 2.8916 18.9157 2.2892 18.3133 1.6867C17.7108 1.0843 17.2289 0.8434 16.506 0.4819C15.9036 0.241 15.1807 0.1205 14.0964 0C13.012 0 12.7711 0 10 0Z" fill="#FFF6E1"/>
+<path d="M10 1.8072H14.0964C15.0602 1.8072 15.5422 2.0482 15.9036 2.1687C16.3855 2.4096 16.747 2.5301 17.1084 2.8916C17.4699 3.253 17.7108 3.6145 17.8313 4.0964C17.9518 4.4578 18.0723 4.9398 18.1928 5.9036V10V14.0964C18.1928 15.0602 17.9518 15.5422 17.8313 15.9036C17.5904 16.3855 17.4699 16.747 17.1084 17.1084C16.747 17.4699 16.3855 17.7108 15.9036 17.8313C15.5422 17.9518 15.0602 18.0723 14.0964 18.1928H10H5.9036C4.9398 18.1928 4.4578 17.9518 4.0964 17.8313C3.6145 17.5904 3.253 17.4699 2.8916 17.1084C2.5301 16.747 2.2892 16.3855 2.1687 15.9036C2.0482 15.5422 1.9277 15.0602 1.8072 14.0964V10V5.9036C1.8072 4.9398 2.0482 4.4578 2.1687 4.0964C2.4096 3.6145 2.5301 3.253 2.8916 2.8916C3.253 2.5301 3.6145 2.2892 4.0964 2.1687C4.4578 2.0482 4.9398 1.9277 5.9036 1.8072H10Z" fill="#FFF6E1"/>
+<path d="M15.1089 5C15.6612 5 16.1089 4.5523 16.1089 4C16.1089 3.4477 15.6612 3 15.1089 3C14.5566 3 14.1089 3.4477 14.1089 4C14.1089 4.5523 14.5566 5 15.1089 5Z" fill="#175445"/>
+<path d="M10 14C12.2091 14 14 12.2091 14 10C14 7.79086 12.2091 6 10 6C7.79086 6 6 7.79086 6 10C6 12.2091 7.79086 14 10 14Z" stroke="#175445" stroke-width="2"/>
+</svg>
+
+                  </span>
+                  <span v-else>
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M19.6 10.2271C19.6 9.51801 19.5364 8.83619 19.4182 8.18164H10V12.0498H15.3818C15.15 13.2998 14.4455 14.3589 13.3864 15.068V17.5771H16.6182C18.5091 15.8362 19.6 13.2726 19.6 10.2271Z"
+                        fill="#FFF6E1"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M9.99961 19.9999C12.6996 19.9999 14.9633 19.1045 16.6178 17.5772L13.386 15.0681C12.4905 15.6681 11.3451 16.0226 9.99961 16.0226C7.39506 16.0226 5.19051 14.2635 4.40415 11.8999H1.06323V14.4908C2.70869 17.759 6.09052 19.9999 9.99961 19.9999Z"
+                        fill="#FFF6E1"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M4.40455 11.8997C4.20455 11.2997 4.09092 10.6588 4.09092 9.99971C4.09092 9.34062 4.20455 8.6997 4.40455 8.0997V5.50879H1.06364C0.386364 6.85879 0 8.38607 0 9.99971C0 11.6133 0.386364 13.1406 1.06364 14.4906L4.40455 11.8997Z"
+                        fill="#FFF6E1"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M9.99961 3.97728C11.4678 3.97728 12.786 4.48183 13.8224 5.47274L16.6905 2.60455C14.9587 0.990911 12.6951 0 9.99961 0C6.09052 0 2.70869 2.24091 1.06323 5.5091L4.40415 8.10002C5.19051 5.73638 7.39506 3.97728 9.99961 3.97728Z"
+                        fill="#FFF6E1"
+                      />
+                    </svg>
+                  </span>
                 </span>
                 <span class="reviews-section__author-copy">
-                  <strong>{{ selectedReview.author }}, {{ getReviewLocation(selectedReview) }}</strong>
+                  <strong
+                    >{{ selectedReview.author }}, {{ getReviewLocation(selectedReview) }}</strong
+                  >
                   <span>{{ getReviewDate(selectedReview) }}</span>
                 </span>
               </span>
